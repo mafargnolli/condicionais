8a0f180ed18eedc0b4171713aa209e0d905a80d2
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-12 19:54:55 -0300

    Exercicio 16 condicionais, rematricula, parte b

--- 16.js
+++ 16.js
@@ -1,15 +1,27 @@
 //a) Faça um programa que verifique se o aluno é maior de idade e realize a rematrícula, caso não seja, só será possível acompanhado de um responsável.
 
 //Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
+//const idadeDoAluno = 16;
+
+//valor verdadeiro ou falso que informa se possui responsável
+//const possuiResponsavel = false;
+
+//if (idadeDoAluno >= 18){;
+//    console.log("Pode efetuar a rematricula");
+//} else if(possuiResponsavel != false){
+//   console.log("Possui responsavel, pode rematricular!");
+//}else {
+//    console.log("Nao pode se rematricular");
+//}
+
+//Só é considerado de maior se a idade for maior ou igual a 18.
+const idadeDoAluno = 18;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
 
-if (idadeDoAluno >= 18){;
-    console.log("Pode efetuar a rematricula");
-} else if(possuiResponsavel != false){
-    console.log("Possui responsavel, pode rematricular!");
-}else {
-    console.log("Nao pode se rematricular");
+if (idadeDoAluno >=18) {
+    console.log("Pode efetuar a rematricula!");
+} else if (possuiResponsavel == false){
+    console.log("Nao pode se rematricular!");
 }
