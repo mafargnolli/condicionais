4f54f39d3976a63d0b5340621d922cdde2c97f23
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-12 16:26:15 -0300

    Exercicio criterio volei

---
+++ 5.js
@@ -0,0 +1,9 @@
+//altura minima eh de 180 cm para ser aprovado, o exercicio quer saber se esta reprovado ou aprovado na peneira de volei.
+
+const alturaEmCm = 185;
+
+if(alturaEmCm >= 180) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
