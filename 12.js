541302b67c008fbc5f7c6e4fd707e82593216720
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-12 18:19:07 -0300

    Parte B exercicio semana

--- 12.js
+++ 12.js
@@ -1,7 +1,7 @@
 //a) Sabendo que a semana possui 7 dias, sendo 1 para segunda e 7 para domingo, faça um programa que verifica o número armazenado em uma variável 
 //e imprima o dia (por extenso) da semana correspondente.
 
-const diaDaSemana = 3;
+const diaDaSemana = 10;
 if (diaDaSemana == 1){
     console.log("Segunda-feira");
 }else if(diaDaSemana == 2){
@@ -16,5 +16,10 @@ if (diaDaSemana == 1){
     console.log("Sabado");
 }else if(diaDaSemana === 7){
     console.log("Domingo");
+} else{
+    console.log("Nao eh um dia correspondente a semana!");
 }
+//b) Crie uma verificação padrão para caso o número informado não esteja no intervalo de 1 a 7.
+
+
 
