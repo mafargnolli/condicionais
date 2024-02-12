9538a154b7b9370253144306d04b9a0600a79234
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-12 19:41:15 -0300

    Truthiness exercicio condicionais 15

---
+++ 15.js
@@ -0,0 +1,12 @@
+//Uma pessoa pode ter nome, sobrenome e/ou apelido. Escreva um código que imprima no console apenas o apelido (se o apelido estiver preenchido), 
+//ou primeiro nome + sobrenome (se o sobrenome estiver preenchido), ou apenas o primeiro nome (que no nosso programa é obrigatório e deverá sempre estar preenchido).
+
+//Neste exercício, não deveremos utilizar nenhum operador de comparação, nem de igualdade e nem de identidade. 
+//Para este exercício deveremos utilizar apenas o conceito de truthiness.
+
+const primeiroNome = "Mario";
+const sobrenome = "";
+const apelido = "";
+
+if (primeiroNome);
+console.log("Mario");
